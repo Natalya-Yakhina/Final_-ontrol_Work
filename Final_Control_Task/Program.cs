bc239c70a4eee5e43a7a65f317800a8468434990
Яхина Наталья Андреевна <  @vvrz.local>
2022-08-05 16:47:20 +0300

    final

--- Final_Control_Task/Program.cs
+++ Final_Control_Task/Program.cs
@@ -5,137 +5,38 @@
 //ПРИМЕРЫ:
 // ["hello", "2", "world", ":-)"-> ["2", ":-)"] 
 // ["1234", "1567", "-2", "computer science"] -> ["-2"] 
-// ["Russia”, "Denmark", "Kazan"] -> [ ]
-
-
-// string[] CreateArray (string[] array) // вывод массива
-// {
-//    int count = 0; // переменная для подсчета длины нового массива
-//    for (int i = 0; i < array.Length; i++)
-//    {
-//       if (array[i].Length <= 3)
-//       count++;
-//    }
-
-//    string[] arrayNew = new string[count];
-//    // инициализация 2-го массива с длиной, которую выше определили
-//    Console.Write($"Новый массив: [");
-
-//    int j = 0;
-//    for (int i = 0; i < array.Length; i++)
-//    {
-//       if (array[i].Length <= 3) // проверка по заданному условию
-//       {
-//          arrayNew[j] = array[i]; // присвоение подходящего элемента
-//          Console.Write($"{arrayNew[j]} ");
-//          j++; // увеличение индекса у второго массива
-//       }
-//    }
-//    Console.Write($"]");
-//    return arrayNew;
-// }  
-
-// string[] initialarray = {"hello", "2", "world", ":-)"};
-// //string[] initialarray = {"hello", "2", "world", ":-)"};   for test
-// //string[] initialarray = {"Russia", "Denmark", "Kazan"};   for test
-// CreateArray (initialarray); 
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// string[] FindLengthString(int lenElem, string[] arrayInput) // метод поиска длинны строки
-// {
-//     int len = arrayInput.Length;    // Измерил длину массива и записал в переменную
-//     int count = 0; // Заводим переменную счётчик
-//     for (int i = 0; i < len; i++)  // В этом цикле считаем количество строк в массиве нужной длины
-//     {
-//         if (arrayInput[i].Length <= lenElem)
-//         {
-//             count++;
-//         }
-//     }
-//     // Создаём новый массив в который будем записывать строки нужной длины
-//     string[] resultArray = new string[count];
-//     count = 0;
-//     // В этом цикле берём определённые элементы из старого массива
-//     // и записываем их в новый
-//     for (int i = 0; i < len; i++)
-//     {
-//         if (arrayInput[i].Length <= lenElem)
-//         {
-//             resultArray[count] = arrayInput[i];
-//             count++;
-//         }
-//     }
-//     return resultArray;
-// }
-
-// // Сделаем метод, который будет печатать результирующий массив в консоль
-// void PrintArray(string[] array)
-// {
-//     Console.Write("[");
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (i == array.Length - 1)
-//         {
-//             Console.Write(array[i]);
-//         }
-//         else
-//         {
-//             Console.Write(array[i] + ", ");
-//         }
-//     }
-//     Console.WriteLine("]");
-// }
-
-// // В эту переменную записывается не большу какой длины должны быть
-// // элементы результирующего массива
-// int lenElemArray = 3;
-// // Печатаем три примера работы метода
-// Console.WriteLine("Максимальный размер элементов в массиве: " + lenElemArray);
-// string[] array1 = new string[] {"hello", "2", "world", ":-)"};
-// PrintArray(FindLengthString(lenElemArray, array1));
-// string[] array2 = new string[] {"1234", "1567", "-2", "computer science"};
-// PrintArray(FindLengthString(lenElemArray, array2));
-// string[] array3 = new string[] {"Russia", "Denmark", "Kazan"};
-// PrintArray(FindLengthString(lenElemArray, array3));
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ["Russia", "Denmark", "Kazan"] -> [ ]
 
 Console.Clear();
-string[] CreateArray(int lengthElement, string[] array)
+string[] stringArray = {"hello", "2", "world", ":-)"}; // Создание и заполнение первоначального массива
+// string[] stringArray = {"1234", "1567", "-2", "computer science"};
+// string[] stringArray = {"Russia", "Denmark", "Kazan"};
+string[] stringNewArray = new string[stringArray.Length]; // Новый массив
+
+void FillArray(string[] stringArray, string[] stringNewArray) // Метод, заполняющий новый массив строками, в которых <= 3 символа из первоначального массива
 {
-    int length = array.Length; // длинна массива
-    int count = 0; // переменная счётчик
-    for (int i = 0; i < length; i++)  // считаем количество строк в массиве нужной длины
+    int count = 0;
+    for (int i = 0; i < stringArray.Length; i++)
     {
-        if (array[i].Length <= lengthElement)
+        if (stringArray[i].Length <= 3)
         {
+            stringNewArray[count] = stringArray[i];
             count++;
         }
     }
-    // Создаём новый массив в который будем записывать строки нужной длины
-    string[] arrayNew = new string[count];
-    count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i].Length <= lengthElement)
-        {
-            arrayNew[count] = array[i];
-            count++;
-        }
-    }
-    return arrayNew;
 }
 
-void PrintArray(string[] arr) // вывод массива
+void PrintArray(string[] array) // Метод вывода массива на печать
 {
-    for (int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if (i == 0) Console.Write("[");
-        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
-        else Console.Write(arr[i] + "]");
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
 
-int lengthElement = 3;
-Console.WriteLine("Максимальный размер элементов в массиве: " + lengthElement);
-string[] array = new string[] { "hello", "2", "world", ":-)" };
-PrintArray(CreateArray(lengthElement, array));
+FillArray(stringArray, stringNewArray);
+Console.Write("Исходный массив: ");
+PrintArray(stringArray);
+Console.Write("Новый массив: ");
+PrintArray(stringNewArray);
